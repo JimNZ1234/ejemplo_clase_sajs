887e2d71fb8e9240ca3e8354396bb18cecf2bcb4
JimNZ1234 <sebastian.jimenez_s@uao.edu.co>
2025-05-14 15:12:45 -0500

    Añadir los primeros archivos

---
+++ visualizacion.py
@@ -0,0 +1,15 @@
+#Librerias:
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+#Seaborn para visualizacion de datos
+import seaborn as sns
+import os
+from analisis import DataAnalyzer
+
+data = pd.read_csv("adult.csv")
+analizar = DataAnalyzer(data)
+
+analizar.summary()
+analizar.correlation_matrix()
+analizar.categorical_analisis
